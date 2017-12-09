d804205cdbdb27890fbc11c5db842d9916216557
roberto-equals <roberto.goncalves@equals.com.br>
2017-12-09 21:20:51 -0200

    accelerometer

--- backend/acelerometro.py
+++ backend/acelerometro.py
@@ -4,12 +4,11 @@ def accelerometer(data):
         acValue = firstData["ac"]
         #Abaixando a cabeca fica -6, Levantando a cabeca fica 6
         yValue = str(acValue["y"]).split('.')[0]
+        yValue = int(yValue)
         if(yValue <= -6 and yValue >= 6):
             if(yValue <= -6):
                 firstData["status"]["head"] = -1
             else:
                 firstData["status"]["head"] = 1
-        else:
-            firstData["status"]["head"] = 0
-    print data
+    print(data)
     return "hi"
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import firebase_admin
-from cow import analizer
+from distance_analyzer import analizer
 from acelerometro import accelerometer
 from firebase_admin import credentials, db
 
